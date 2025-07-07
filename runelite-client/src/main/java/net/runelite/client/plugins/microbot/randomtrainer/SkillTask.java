67829e0a3be5d2768d63da80c67ad1524de23926
KSPOG <kratosmaximusosrs@gmail.com>
2025-07-08 01:51:39 +0200

    Fix SkillTask enum formatting

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/randomtrainer/SkillTask.java
@@ -0,0 +1,15 @@
+// Skill tasks for Random Trainer plugin
+package net.runelite.client.plugins.microbot.randomtrainer;
+
+public enum SkillTask {
+    MINING,
+    SMITHING,
+    CRAFTING,
+    WOODCUTTING,
+    RUNECRAFTING,
+    FIREMAKING,
+    FISHING,
+    COOKING,
+    RANGED,
+    PRAYER;
+}
