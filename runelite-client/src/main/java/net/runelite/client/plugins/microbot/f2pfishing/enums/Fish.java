cf1e66d031e57f10c74ea0afbd8520ec515d2ede
KSPOG <kratosmaximusosrs@gmail.com>
2026-02-07 19:45:46 +0100

    F2P
    Fishing

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/f2pfishing/enums/Fish.java
@@ -0,0 +1,37 @@
+package net.runelite.client.plugins.microbot.f2pfishing.enums;
+
+import net.runelite.api.ItemID;
+
+public enum Fish {
+    SHRIMP("Shrimp", ItemID.RAW_SHRIMPS, ItemID.SHRIMPS),
+    ANCHOVIES("Anchovies", ItemID.RAW_ANCHOVIES, ItemID.ANCHOVIES),
+    SARDINE("Sardine", ItemID.RAW_SARDINE, ItemID.SARDINE),
+    HERRING("Herring", ItemID.RAW_HERRING, ItemID.HERRING),
+    TROUT("Trout", ItemID.RAW_TROUT, ItemID.TROUT),
+    SALMON("Salmon", ItemID.RAW_SALMON, ItemID.SALMON),
+    TUNA("Tuna", ItemID.RAW_TUNA, ItemID.TUNA),
+    LOBSTER("Lobster", ItemID.RAW_LOBSTER, ItemID.LOBSTER),
+    SWORDFISH("Swordfish", ItemID.RAW_SWORDFISH, ItemID.SWORDFISH);
+
+    private final String name;
+    private final int rawItemId;
+    private final int cookedItemId;
+
+    Fish(String name, int rawItemId, int cookedItemId) {
+        this.name = name;
+        this.rawItemId = rawItemId;
+        this.cookedItemId = cookedItemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRawItemId() {
+        return rawItemId;
+    }
+
+    public int getCookedItemId() {
+        return cookedItemId;
+    }
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/f2pfishing/enums/FishingMethod.java
@@ -0,0 +1,21 @@
+package net.runelite.client.plugins.microbot.f2pfishing.enums;
+
+public enum FishingMethod {
+    SMALL_NET("Small net"),
+    BIG_NET("Big net"),
+    NET("Net"),
+    BAIT("Bait"),
+    LURE("Lure"),
+    CAGE("Cage"),
+    HARPOON("Harpoon");
+
+    private final String displayName;
+
+    FishingMethod(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
